c8a2503b83762e0aaccfb86e33cb2b2ca1c4e3d1
Aleksey Sazonov <saleksvik@gmail.com>
2018-03-18 17:12:46 +0300

    g++-6 in .travis.yml

--- lib_helloworld.cpp
+++ lib_helloworld.cpp
@@ -7,7 +7,7 @@
 std::string version()
 {
 	return std::to_string(PROJECT_VERSION_MAJOR)
-		   + "." + std::to_string(PROJECT_VERSION_MINOR)
-		   + "." + std::to_string(PROJECT_VERSION_PATCH);
+           + "." + std::to_string(PROJECT_VERSION_MINOR)
+           + "." + std::to_string(PROJECT_VERSION_PATCH);
 }
 
