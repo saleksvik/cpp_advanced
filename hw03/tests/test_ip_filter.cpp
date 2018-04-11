3b6ced9ba0b23e1e3f2ca8f0b098e6fddd7a4062
Aleksey Sazonov <saleksvik@gmail.com>
2018-04-11 23:16:44 +0300

    Changed test

--- hw03/tests/test_ip_filter.cpp
+++ hw03/tests/test_ip_filter.cpp
@@ -27,7 +27,7 @@ std::ostream& operator<< (std::ostream& ostr, ip_t const& ip_part)
 
 namespace boost {
     namespace test_tools {
-        namespace tt_detail{
+//        namespace tt_detail{
             template<>
             struct print_log_value<ip_t> {
                 void operator()( std::ostream& os, ip_t const& ts)
@@ -36,7 +36,7 @@ namespace boost {
                 }
              };
         }
-    }
+//    }
 }
 
 namespace tt = boost::test_tools;
